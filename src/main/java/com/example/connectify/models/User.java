93da6fcb30c785ecacf3c3b2deb893afda10fbfa
fejesmartin <fejes.martin00@gmail.com>
2024-03-19 10:43:31 +0100

    feature: adds User model

---
+++ src/main/java/com/example/connectify/models/User.java
@@ -0,0 +1,27 @@
+package com.example.connectify.models;
+import com.example.connectify.enums.Role;
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    private boolean enabled;
+
+}
