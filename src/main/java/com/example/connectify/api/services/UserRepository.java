867471e7483edf265ddca3ec2751147d9ae0e6ca
fejesmartin <fejes.martin00@gmail.com>
2024-03-21 22:08:47 +0100

    feature: adds new methods to UserRepository class

--- src/main/java/com/example/connectify/api/services/UserRepository.java
+++ src/main/java/com/example/connectify/api/services/UserRepository.java
@@ -2,6 +2,12 @@ package com.example.connectify.api.services;
 
 import com.example.connectify.api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> getUserByUsername(String username);
+    Optional<User> getUserByEmail(String email);
 }
