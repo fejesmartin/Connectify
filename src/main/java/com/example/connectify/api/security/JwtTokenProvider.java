a92bbd241227e04036eda6051d30eb6a647d1c31
fejesmartin <fejes.martin00@gmail.com>
2024-03-21 22:05:17 +0100

    feature: reimplements JwtTokenProvider class

--- src/main/java/com/example/connectify/api/security/JwtTokenProvider.java
+++ src/main/java/com/example/connectify/api/security/JwtTokenProvider.java
@@ -1,70 +1,50 @@
 package com.example.connectify.api.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.Collectors;
 
-@Component
-@RequiredArgsConstructor
+@Service
 public class JwtTokenProvider {
+    @Autowired
+    private JwtEncoder jwtEncoder;
 
-    @Value("${jwt.secret}")
-    private String secret;
+    @Autowired
+    private JwtDecoder jwtDecoder;
 
-    @Value("${jwt.expiration}")
-    private int expiration;
+    public String generateJwt(Authentication auth, Long id)
+    {
+        Instant now = Instant.now();
+        Duration validityDuration = Duration.ofMinutes(30);
+        Instant expirationTime = now.plus(validityDuration);
 
-    public String generateToken(Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, userDetails.getUsername());
+        String scope = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .subject(String.valueOf(id))
+                .expiresAt(expirationTime)
+                .claim("roles", scope)
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 
-    private String createToken(Map<String, Object> claims, String subject) {
-        Date now = new Date();
-        Date expirationDate = new Date(now.getTime() + expiration * 1000L);
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(subject)
-                .setIssuedAt(now)
-                .setExpiration(expirationDate)
-                .signWith(SignatureAlgorithm.HS256, secret)
-                .compact();
-    }
+    public Jwt decodeJwt(String jwt)
+    {
+        var decode = jwtDecoder.decode(jwt);
+        System.out.println(decode.getSubject());
+        System.out.println(decode.getClaims());
 
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
-
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    public Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
-    }
-
-    private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
+        return decode;
     }
 }
