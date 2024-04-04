578da92c787b43bc3de4fe7a4105dab5f29dc075
fejesmartin <fejes.martin00@gmail.com>
2024-04-04 19:12:02 +0200

    fix: changes errors for better error handling

--- ConnectifyApp/src/main/java/com/example/connectify/api/services/CommentService.java
+++ ConnectifyApp/src/main/java/com/example/connectify/api/services/CommentService.java
@@ -5,7 +5,9 @@ import com.example.connectify.api.models.CommentDTO;
 import com.example.connectify.api.models.Post;
 import com.example.connectify.api.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class CommentService {
@@ -22,8 +24,10 @@ public class CommentService {
     public Comment createComment(CommentDTO commentDTO) {
         // Convert DTO to entity
         Comment comment = new Comment();
-        User author = userRepository.findById(commentDTO.getAuthorId()).orElseThrow(() -> new RuntimeException("User not found"));
-        Post post = postRepository.findById(commentDTO.getPostId()).orElseThrow(() -> new RuntimeException("Post not found"));
+        User author = userRepository.findById(commentDTO.getAuthorId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+        Post post = postRepository.findById(commentDTO.getPostId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found"));
 
         comment.setAuthor(author);
         comment.setPost(post);
@@ -36,21 +40,23 @@ public class CommentService {
     }
 
     public Comment getCommentById(Long commentId) {
-        return commentRepository.findById(commentId).orElseThrow(() -> new RuntimeException("Comment not found"));
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Comment not found"));
     }
 
     public Comment updateCommentById(Long commentId, CommentDTO commentDTO){
-        Comment existingComment = commentRepository.findById(commentId).orElseThrow(()-> new RuntimeException("Comment not found"));
+        Comment existingComment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Comment not found"));
         if(commentDTO.getContent() != null){
             existingComment.setContent(commentDTO.getContent());
         }
         System.out.println("Comment updated");
         return commentRepository.save(existingComment);
-
     }
 
     public void deleteCommentById(Long commentId){
-        Comment existingComment = commentRepository.findById(commentId).orElseThrow(()-> new RuntimeException("Comment not found"));
+        Comment existingComment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Comment not found"));
 
         commentRepository.delete(existingComment);
         System.out.println("Comment deleted");
