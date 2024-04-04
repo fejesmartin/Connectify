cab5375d3fe797fba342fa35eb01d9af2ae2cb1f
fejesmartin <fejes.martin00@gmail.com>
2024-04-04 10:25:11 +0200

    feature: implements CommentRepository interface

---
+++ src/main/java/com/example/connectify/api/services/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.connectify.api.services;
+
+import com.example.connectify.api.models.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
