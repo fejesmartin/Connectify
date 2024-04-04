d5e6730645a163952067e06e9dcd4eee84b36ba4
fejesmartin <fejes.martin00@gmail.com>
2024-04-04 10:25:41 +0200

    feature: implements PostDTO model

---
+++ src/main/java/com/example/connectify/api/models/PostDTO.java
@@ -0,0 +1,14 @@
+package com.example.connectify.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDTO {
+    private Long authorId;
+    private String content;
+
+}
