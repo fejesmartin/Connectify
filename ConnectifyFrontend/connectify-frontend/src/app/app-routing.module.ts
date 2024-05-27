4ef69c76b2ed5c69731e37f9aa506c08b73dbd03
fejesmartin <fejes.martin00@gmail.com>
2024-05-27 08:26:31 +0200

    chore: adds user-profile component to routing

--- ConnectifyFrontend/connectify-frontend/src/app/app-routing.module.ts
+++ ConnectifyFrontend/connectify-frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { PostfeedComponent } from './postfeed/postfeed.component';
 import { FeedComponent } from './feed/feed.component';
 import { VisitUserProfileComponent } from './visit-user-profile/visit-user-profile.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'postfeed', component: PostfeedComponent },
   { path: 'feed', component: FeedComponent },
-  { path: 'visit/:username', component: VisitUserProfileComponent }
+  { path: 'visit/:username', component: VisitUserProfileComponent },
+  { path: 'profile', component: UserProfileComponent }
 ];
 
 @NgModule({
