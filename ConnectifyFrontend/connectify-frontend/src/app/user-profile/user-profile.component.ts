62b6dc3a6c8d9b6d92c0d1c0f860d4234f135e5d
fejesmartin <fejes.martin00@gmail.com>
2024-05-30 13:01:27 +0200

    feature: user-profile can now recieve the logged in username

--- ConnectifyFrontend/connectify-frontend/src/app/user-profile/user-profile.component.ts
+++ ConnectifyFrontend/connectify-frontend/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+  username: string|null = '';
 
-  ngOnInit() {
-  }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.username = params.get("username");
+
+    })
+  };
 
 }
