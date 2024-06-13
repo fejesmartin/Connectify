6abc80b39c10b75f853e8a696249cf14a82e871f
fejesmartin <fejes.martin00@gmail.com>
2024-06-13 08:38:42 +0200

    fix: minor name change and remove comment

--- ConnectifyFrontend/connectify-frontend/src/app/app.component.ts
+++ ConnectifyFrontend/connectify-frontend/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] // Use styleUrls instead of styleUrl
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'connectify-frontend';
